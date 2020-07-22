ac88eaa563f90411399f6c8303a6a257a42cfa36
Chibuzor Nwankwo <mackingnwanks27@gmail.com>
2020-07-22 12:04:03 +0100

    Finalizing the burger menu and also adding animations to the buttons.

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,35 @@
-console.log("Hello World");
+const btnhamburger = document.getElementById("btnHamburger");
+const overlay = document.getElementById("overlay");
+
+btnhamburger.addEventListener("click", function(){
+    btnhamburger.classList.toggle("open");
+    overlay.classList.toggle("overlay");
+
+});
+
+
+    // if(btnhamburger.classList.contains('open')){
+    //     btnhamburger.classList.remove("open");
+    //     overlay.classList.remove("overlay");
+    //     overlay.classList.add("fade-out");
+
+    // }
+    // else {
+    //     btnhamburger.classList.add("open");
+    //     overlay.classList.add("overlay");
+    // }
+
+
+// Original Tried menu
+
+// const hmenu = document.querySelector('#hmenu');
+
+// hmenu.addEventListener('click', function(){
+
+//     if(hmenu.classList.contains('open')){
+//         hmenu.classList.remove('open');
+//     }
+//     else {
+//         hmenu.classList.add('open');
+//     }
+// });
